2adfdc3488adb07ad0d021a768cafbec4cecd62b
PointlessBox <am.fabian@mailbox.org>
2022-01-04 16:26:37 +0100

    added tabs to switch between table and chart; added achievement component and test button for it

--- bin-client/src/App.js
+++ bin-client/src/App.js
@@ -1,14 +1,69 @@
 import "antd/dist/antd.css";
+import { message, Row, Col, Tabs, Icon, Button } from 'antd';
+import { TableOutlined, LineChartOutlined, PictureOutlined } from '@ant-design/icons';
 import StatisticTable from "./components/StatisticTable";
 import {startFileWatcher} from "./utils/FileWatcher";
 import StatisticChart from "./components/StatisticChart";
+import Achievement from "./components/Achievement";
+
+const { TabPane } = Tabs;
+
+const showAchievement= ({ img, points, title, description }) => {
+    // TODO: Show img prop
+    message.open({
+        content: <Achievement points={points} title={title} description={description} />,
+        icon: <PictureOutlined />
+    })
+}
+
+const testAchievement = `{
+    "img": "PsyschoAndreasRTL",
+    "points": 1,
+    "title": "Alles bleibt wie es ist",
+    "description": "Verbrauch +- 5% im Vergleich zur Vorwoche",
+    "trigger": " "
+  }`
 
 function App() {
     startFileWatcher();
+
+    // TODO: Add file watcher that shows achievements when smth happens
+
     return (
         <div>
-            <StatisticTable/>
-            <StatisticChart/>
+            <Tabs size="large" defaultActiveKey="1" centered>
+                <TabPane
+                    tab={
+                        <span>
+                            <TableOutlined />
+                            Table
+                        </span>
+                    }
+                    key="1"
+                >
+                    <Row>
+                        <Col span={1}/>
+                        <Col span={22}><StatisticTable/></Col>
+                        <Col span={1}/>
+                    </Row>
+                </TabPane>
+                <TabPane
+                    tab={
+                        <span>
+                            <LineChartOutlined />
+                            Chart
+                        </span>
+                    }
+                    key="2"
+                >
+                    <Row>
+                        <Col span={1}/>
+                        <Col span={22}><StatisticChart/></Col>
+                        <Col span={1}/>
+                    </Row>
+                </TabPane>
+            </Tabs>
+            <Button onClick={() => showAchievement(JSON.parse(testAchievement))}>SHOW!</Button>
         </div>
     );
 }
