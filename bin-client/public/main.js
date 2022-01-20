e977a6dbceb8c4e5aebfbed182ea8422a7cdb02a
Aldin <aldin.hajdarpasic@sp-con.de>
2022-01-20 12:59:35 +0100

    created build parameters

--- bin-client/public/main.js
+++ bin-client/public/main.js
@@ -1,5 +1,8 @@
-const {app, BrowserWindow} = require('electron')
 require("@electron/remote/main").initialize()
+
+const {app, BrowserWindow} = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
 
 function createWindow(){
 
@@ -13,7 +16,7 @@ function createWindow(){
         }
     });
 
-    window.loadURL("http://localhost:3000")
+    window.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname,'../build/index.html')}`)
 }
 
 app.on("ready",createWindow)
